6491925704d976ac717ce3694f3039a5ba858ce5
Mahdi Shekari <mahdishekari7@gmail.com>
2023-12-13 21:34:16 +0330

    chs

--- src/Components/Cost.jsx
+++ src/Components/Cost.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Cost = ({data}) => {
+
+    //alert("Rerender Shodam");
+
+   const [myPrice, setMyPrice] = useState(data.price);
     // const a = {data : "ffsf", blah: "abcd"}
     // const {data} = a;
+    //let myPrice = data.price;
+
+    const clickHandler = () =>{
+        //alert("click shod!!!");
+        //myPrice = 100;
+        //alert(myPrice);
+        setMyPrice(myPrice+1);
+    }
   return (
     <div>
         <h1>{data.type}</h1>
-        <h1>{data.price}</h1>
+        <h1>{myPrice}</h1>
+        <button onClick={clickHandler}>change price</button>
     </div>
   )
 }
